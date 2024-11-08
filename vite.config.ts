2d4a142e03f3bca3dbadd3fccd581850cd8a5b6f
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-11-08 00:28:45 -0300

    corrigindo rota do index

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: 'dist', // Garante que o build seja gerado na pasta 'dist'
+    assetsDir: 'assets', // Pastas para recursos estáticos (como imagens, CSS, etc.)
+  },
 })
